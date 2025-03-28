78248f75c86bdaea1a56e187e690a096c0f9247f
maciek <maciek29.opozda@gmail.com>
2025-03-28 22:44:21 +0100

    PR- Test optimisation

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ function App() {
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
+            <p>Udalo sie zdeployowac</p>
         </p>
       </div>
       <p className="read-the-docs">
