4e8d944cf527096ef7b5e6030dd7387fe79719bf
maciek <maciek29.opozda@gmail.com>
2025-03-31 21:25:55 +0200

    PR-10 Trigger test chane

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,16 @@ import './App.css'
 
 function App() {
     return (
+        <>
         <h1>Hello Appp Need to change you</h1>
+            <h2>We can trust this app</h2>
+            <div
+            style={{width:'300px', height:'300px', backgroundColor:'red'}}
+            >
+                <span>Hello i am span</span>
+            </div>
+        </>
+
     )
 }
 
